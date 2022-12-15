2a9196a89f465ea355164ebd12ec899c296c2631
Gabriel447 <Gabriel_447@live.com>
2022-12-15 16:03:51 -0300

    notification-examples

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,30 @@
+export interface NotificationProps {
+    content: string;
+    category: string;
+}
+
+export class Notification {
+    private props: NotificationProps;
+
+    constructor(props: NotificationProps) {
+        this.props = props;
+    }
+
+    public set content(content: string) {
+        if (content.length >= 1) {
+            throw new Error();
+        }
+
+        this.props.content = content;
+    }
+
+    public get content(): string {
+        return this.props.content;
+    }
+}
+
+const notification = new Notification({
+    content: 'asasda',
+    category: 'social',
+})
+
