2f08a4f0bf9d445167cb3943f1983af029fc6bbf
Gabriel447 <Gabriel_447@live.com>
2022-12-15 19:16:45 -0300

    memory-repository

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,5 +1,4 @@
 import { InMemoryNotificationsRepository } from "../../../test/repositories/in-memory-notifications-repository";
-import { Notification } from "../entities/notification";
 import { SendNotification } from "./send-notification";
 
 describe('Send notification', () => {
@@ -7,12 +6,13 @@ describe('Send notification', () => {
         const notificationsRepository = new InMemoryNotificationsRepository();
         const sendNotification = new SendNotification(notificationsRepository);
 
-        await sendNotification.execute({
+        const { notification } = await sendNotification.execute({
             content: 'This is a notification',
             category: 'social',
             recipientId: 'example-recipient-id',
         });
 
         expect(notificationsRepository.notifications).toHaveLength(1);
+        expect(notificationsRepository.notifications[0]).toEqual(notification);
     });
 });
