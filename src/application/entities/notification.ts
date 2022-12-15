6785b95d6887d435ba54cf0aaaef415bd7be5567
Gabriel447 <Gabriel_447@live.com>
2022-12-15 16:18:27 -0300

    gettersNsetters

--- src/application/entities/notification.ts
+++ src/application/entities/notification.ts
@@ -1,6 +1,9 @@
 export interface NotificationProps {
+    recipientId: string;
     content: string;
     category: string;
+    readAt?: Date | null;
+    createdAt: Date;
 }
 
 export class Notification {
@@ -10,21 +13,39 @@ export class Notification {
         this.props = props;
     }
 
-    public set content(content: string) {
-        if (content.length >= 1) {
-            throw new Error();
-        }
+    public set recipientId(recipientId: string) {
+        this.props.recipientId = recipientId;
+    }
 
+    public get recipientId(): string {
+        return this.props.recipientId;
+    }
+
+    public set content(content: string) {
         this.props.content = content;
     }
 
     public get content(): string {
         return this.props.content;
     }
+
+    public set category(category: string) {
+        this.props.category = category;
+    }
+
+    public get category(): string {
+        return this.props.category;
+    }
+
+    public set readAt(readAt: Date | null | undefined) {
+        this.props.readAt = readAt;
+    }
+
+    public get readAt(): Date | null | undefined {
+        return this.props.readAt;
+    }
+
+    public get createdAt(): Date {
+        return this.props.createdAt;
+    }
 }
-
-const notification = new Notification({
-    content: 'asasda',
-    category: 'social',
-})
-
