997a1a1d14dc6fb348ae2f60090c1e09bf40cbea
Gabriel447 <Gabriel_447@live.com>
2022-12-17 19:40:27 -0300

    count-spec

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -2,9 +2,10 @@ import { Content } from "@application/entities/content";
 import { Notification } from "@application/entities/notification";
 import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository";
 import { CancelNotification } from "./cancel-notification";
+import { NotificationNotFound } from "./errors/notification-not-found";
 
 describe('Send notification', () => {
-    it('should be able to send to send a notification', async () => {
+    it('should be able to cancel a notification', async () => {
         const notificationsRepository = new InMemoryNotificationsRepository();
         const cancelNotification = new CancelNotification(notificationsRepository);
 
@@ -22,5 +23,18 @@ describe('Send notification', () => {
 
         //espero que canceledAt seja igual a qualquer informação/objeto do tipo date
         expect(notificationsRepository.notifications[0].canceledAt).toEqual(expect.any(Date));
+    });
+
+    it('should not be able to cancel a non existing notification', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository();
+        const cancelNotification = new CancelNotification(notificationsRepository);
+
+
+
+        expect(() => {
+            return cancelNotification.execute({
+                notificationId: 'fake-notification-id',
+            });
+        }).rejects.toThrow(NotificationNotFound);
     });
 });
