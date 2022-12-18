a24bd5881c7fc4edbc9b84c67004846a13f2eb96
Gabriel447 <Gabriel_447@live.com>
2022-12-17 21:23:38 -0300

    list-recipient

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -3,7 +3,6 @@ import { NotificationsRepository } from "@application/repositories/notifications
 
 export class InMemoryNotificationsRepository
     implements NotificationsRepository {
-
     public notifications: Notification[] = [];
 
     async findById(notificationId: string): Promise<Notification | null> {
@@ -16,6 +15,12 @@ export class InMemoryNotificationsRepository
         }
 
         return notification;
+    }
+
+    async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+        return this.notifications.filter(
+            (notification) => notification.recipientId === recipientId,
+        );
     }
 
     async countManyByRecipientId(recipientId: string): Promise<number> {
